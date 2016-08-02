cdda7b33133f151e7d1caa38a9d637e1a9674bc5
rahul.bhandari <rahul.bhandari@piksel.com>
2016-08-01 23:02:16 -0700

    fix the loop

--- getAllSubstrings.js
+++ getAllSubstrings.js
@@ -1,3 +1,10 @@
+// ===== STRINGS ======
+// Write a function that takes an input string and prints out various substrings as illustrated by the following sample outputs:
+// - input “ab” should print “”, “a”, “b”, “ab"
+// - input “abc” should print “”, “a”, “b”, “c”, “ab”, “ac”, “bc”, “abc"
+// - input “abcd” should print “”, “a”, “b”, “c”, “d”, “ab”, “ac”, “ad”, “bc”, “bd”, “cd”, “abc”, “abd”, “acd”, “bcd”, “abcd"
+// The function can return/print the substrings in any order (no duplicates). And it is acceptable to print each string in a separate line.
+
 function getAllSubstrings (str) {
 	var substrings = {'':true};
 
@@ -22,15 +29,17 @@ function getAllSubstrings (str) {
                 	if(!(substr in substrings)) {
                         substrings[substr] = true;
                     }
+
+                    for(k=1;k<substr.length-1;k++) {
+                        tempSubStr = substr.slice(0,k) + substr.slice(k+1);
+                        if(!(tempSubStr in substrings)) {
+                            substrings[tempSubStr] = true;
+                        }
+                    }
                     
                 }
 
-                for(j=1;j<substr.length-1;j++) {
-                	tempSubStr = substr.slice(0,j) + substr.slice(j+1);
-                	if(!(tempSubStr in substrings)) {
-                        substrings[tempSubStr] = true;
-                    }
-                }
+                
 
                 substr = '';
             }
@@ -45,7 +54,7 @@ function getAllSubstrings (str) {
     
 }
 
-var substringsResult = getAllSubstrings('ABCD');
+var substringsResult = getAllSubstrings('abcde');
 for(key in substringsResult) {
 	console.log(key);
 }
